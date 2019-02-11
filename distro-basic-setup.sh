20e1d224f7bdd2264d8e088f925559c318eb39b3
Robin Wils <mrwils@tutanota.com>
2019-02-11 07:02:23 +0000

    Refactor distro-basic-setup.sh

--- distro-basic-setup.sh
+++ distro-basic-setup.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# This script assumes that your computer is connected to the internet during the
-# execution of it. This was made for devuan/debian netinstall.
+# This script assumes that your computer is connected to the internet during
+# the execution of it. This was made for Devuan netinstall.
 # I am not responsible for broken systems or lost files,
+
+# Different users can have different packages in guix.
+# This script installs all the packages for the non-root user.
+# It also symlinks it to root so that they have the same packages.
+# I install XFCE4 just in case that I have problems with EXWM.
+# This has not happened yet.
 
 # RUN THIS AT YOUR OWN RISK.
 # THIS SCRIPT HAS NOT BEEN TESTED YET.
@@ -11,37 +17,29 @@
 # CONFIG
 # ______
 
-# Packages to install,
-# the script uses guix to install these.
 
-# Guix-binary from https://alpha.gnu.org/gnu/guix/
+# Guix-binary from https://alpha.gnu.org/gnu/guix
 readonly GUIX_BINARY="guix-binary-0.16.0.x86_64-linux.tar.xz"
 
-# Different users can have different packages in guix.
-# This script installs all the packages for the non-root user.
-# It also symlinks it to root so that they have the same packages.
-# I install XFCE4 just in case that I have problems with EXWM.
-# This has not happened yet.
-readonly GUIX_PACKAGES="acpi git icecat keepassxc libreoffice maim mplayer rsync \ 
-testdisk wicd xfce4-panel xfce4-session xrandr"
+# Packages to install
+readonly GUIX_PACKAGES="acpi git icecat keepassxc libreoffice maim mplayer \
+rsync testdisk wicd xfce4-panel xfce4-session xrandr"
 
-# Default packages of the netinstaller which you don't want.
-# Apt will remove those packages.
+# Apt will remove these packages
 readonly PACKAGES_TO_REMOVE="bluetooth bluez laptop-detect popularity-contest \
 vim-common vim-tiny xxd"
 
-# The email address which you use for git
-# You don't have to use a grep but it is useful,
-# just in case that I change my email.
+# The email address which you use for git,
+# I am lazy so I use a grep but you can use a regular email here.
 readonly GIT_EMAIL=$(wget -qO- https://gitlab.com/RobinWils \
-                         | grep -o '[[:alnum:]+\.\_\-]*@[[:alnum:]+\.\_\-]*' \
-                         | tail -1)
+                            | grep -o '[[:alnum:]+\.\_\-]*@[[:alnum:]+\.\_\-]*' \
+                            | tail -1)
 
 # The username of your non-root user
 readonly USERNAME="rmw"
 
-# The prop wifi debian package
-# Leave this empty if you don't need a prop WiFi driver.
+# The prop wifi debian package,
+# leave this empty if you don't need a prop WiFi driver
 readonly PROP_WIFI="firmware-iwlwifi"
 
 # The Debian repo for the prop WiFi drivers
@@ -57,66 +55,76 @@ readonly HIDE_GRUB=true
 readonly GRUB_DISTRIBUTOR="GNU+Devuan+Guix"
 
 # X and Emacs might crash or give errors
-# if you remove one of these packages.
-readonly ESSENTIAL_GUIX_PACKAGES="emacs font-hack font-misc-misc \
-xf86-input-evdev xf86-input-keyboard xf86-input-mouse \
+# if you remove one of these packages
+readonly ESSENTIAL_GUIX_PACKAGES="emacs nss-certs font-hack \
+font-misc-misc xf86-input-evdev xf86-input-keyboard xf86-input-mouse \
 xf86-input-synaptics xf86-video-nouveau xinit"
 # sct" the sct package does not work on guix for some reason
 # but it is essential for my init script
-readonly ESSENTIAL_APT_PACKAGES="ca-certificates curl dirmngr sct"
+readonly ESSENTIAL_APT_PACKAGES="ca-certificates curl dirmngr nscd sct"
 
 
 # SCRIPT
 # ______
+
 
 if [ "$EUID" -ne 0 ]
 then echo "You need to be root to execute this script."
      exit 1
 fi
 
-# Prop WiFi driver
+
+echo -e "\nPreparing the install"
+echo -e "---------------------\n"
+
+
 if ! [ -z "$PROP_WIFI" ]
 then
+    echo "Installing prop WiFi driver..."
     mv /etc/apt/sources.list /etc/apt/sources.list.old
     touch /etc/apt/sources.list
     echo $DEBIAN_REPO >> /etc/apt/sources.list
-    apt update
-    apt -y --no-install-recommends install $PROP_WIFI
+    apt-get update > /dev/null
+    apt-get -y --no-install-recommends \
+            install $PROP_WIFI > /dev/null
     mv /etc/apt/sources.list.old /etc/apt/sources.list
-    apt update
+    apt-get update > /dev/null
 fi
 
-# Install required apt packages
-apt -y --no-install-recommends install $ESSENTIAL_APT_PACKAGES
-# Remove unneeded apt packages
-apt -y purge $PACKAGES_TO_REMOVE
-apt -y autoremove
 
-# Install guix package manager
+echo "Installing essential apt packages..."
+apt-get -y --no-install-recommends \
+        install $ESSENTIAL_APT_PACKAGES > /dev/null
+
+
+echo "Removing unneeded apt packages..."
+apt-get -y purge $PACKAGES_TO_REMOVE > /dev/null
+apt-get -y autoremove --purge > /dev/null
+
+
+echo -e "\nPreparing guix"
+echo -e "--------------\n"
+
+
+if [[ -e "/var/guix" || -e "/gnu" ]]; then
+    echo "A previous Guix installation was found."
+    echo "Exiting script"
+    exit 1
+fi
+
+
+echo "Installing the guix package manager..."
 gpg --keyserver pool.sks-keyservers.net \
-    --recv-keys 3CE464558A84FDC69DB40CFB090B11993D9AEBB5
-wget -O /tmp/guix-binary.tar.xz.sig https://alpha.gnu.org/gnu/guix/$GUIX_BINARY.sig 
-wget -O /tmp/guix-binary.tar.xz https://alpha.gnu.org/gnu/guix/$GUIX_BINARY
-gpg --verify /tmp/guix-binary.tar.xz.sig
-tar --warning=no-timestamp -xf \
-     /tmp/guix-binary.tar.xz -C /tmp &&
-cp -r /tmp/var/guix /var/ && cp -r /tmp/gnu /
-# Make the guix profile available
-mkdir -p ~root/.config/guix
-ln -sf /var/guix/profiles/per-user/root/current-guix \
-~root/.config/guix/current
-GUIX_PROFILE="`echo ~root`/.config/guix/current" ; \
-source $GUIX_PROFILE/etc/profile
-# Create the group and user accounts for build users
-groupadd --system guixbuild
-for i in `seq -w 1 10`;
-  do
-    useradd -g guixbuild -G guixbuild           \
-            -d /var/empty -s `which nologin`    \
-            -c "Guix build user $i" --system    \
-            guixbuilder$i;
-  done
-# Create SysV script for the guix-daemon
+    --recv-keys 3CE464558A84FDC69DB40CFB090B11993D9AEBB5 \
+&> /dev/null
+bash <(curl -s https://git.savannah.gnu.org/cgit/guix.git/plain/etc/guix-install.sh)
+
+
+# THIS DAEMON DOES NOT WORK
+# RUN IT MANUALLY
+# ~root/.config/guix/current/bin/guix-daemon \
+#       --build-users-group=guixbuild &
+echo "Creating SysV script for the guix-daemon..."
 /bin/cat <<EOM >/etc/init.d/guix-daemon
 #!/bin/sh
 ### BEGIN INIT INFO
@@ -137,59 +145,67 @@ SCRIPTNAME=/etc/init.d/guix-daemon
 
 do_start()
 {
-        # /root/.guix-profile/bin/guix-daemon \
-        #--build-users-group=guixbuild 2>/dev/null || return 2
-        /root/.guix-profile/bin/guix-daemon \
-        --build-users-group=guixbuild 2> /var/log/guix.log &
+    /root/.guix-profile/bin/guix-daemon \
+        --build-users-group=guixbuild \
+        2> /var/log/guix.log &
 }
 
 case "$1" in
-  start)
+    start)
         log_action_begin_msg "Setting up GNU Guix daemon"
         do_start
         case "$?" in
-                0|1) log_action_end_msg 0 ;;
-                2) log_action_end_msg 1 ;;
+            0|1) log_action_end_msg 0 ;;
+            2) log_action_end_msg 1 ;;
         esac
         ;;
-  stop|restart|force-reload|status)
+    stop|restart|force-reload|status)
         log_action_begin_msg "Killing GNU Guix daemon"
         killall guix-daemon
         ;;
-  *)
+    *)
         echo "Usage: $SCRIPTNAME start" >&2
         exit 3
         ;;
 esac
 EOM
-chmod +x /etc/init.d/guix-daemon
+
+echo "Starting the guix-daemon..."
+update-rc.d guix-daemon defaults
+chmod a+x /etc/init.d/guix-daemon
 /etc/init.d/guix-daemon start
-# Make sure that other users can use the guix command
-mkdir -p /usr/local/bin
-ln -s /var/guix/profiles/per-user/root/current-guix/bin/guix
-# Setup guix
-su -c 'guix package -i glibc-utf8-locales &&
-export GUIX_LOCPATH=$HOME/.guix-profile/lib/locale &&
-export INFOPATH=$HOME/.guix-profile/share/info${INFOPATH:+:}$INFOPATH &&
-export LC_ALL=en_US.UTF-8 &&
-export PATH=$HOME/.guix-profile/bin:$PATH/.guix-profile/sbin${PATH:+:}$PATH &&
-guix package -i nss-certs &&
-export SSL_CERT_DIR=$HOME/.guix-profile/etc/ssl/certs
-export SSL_CERT_FILE=$HOME/.guix-profile/etc/ssl/certs/ca-certificates.crt &&
-guix refresh &&
-guix pull &&
-guix package -u' $USERNAME
 
-# Install guix packages as non-root user
-su -c "guix package -i $ESSENTIAL_GUIX_PACKAGES $GUIX_PACKAGES" $USERNAME
+echo "Make a guix profile for our user..."
+mkdir /var/guix/profiles/per-user/rmw
+chown rmw /var/guix/profiles/per-user/rmw
 
-# Symlink the guix packages to root
-rm -rf ~/.guix-profile
-ln -sf /home/$USERNAME/.guix-profile ~/.guix-profile
+echo -e "\nUpdating guix (guix pull)..."
+su -c "guix pull" \
+   $USERNAME &> /dev/null
 
-# Update grub
+echo "Configuring the locales...."
+su  -c 'guix package -i glibc-utf8-locales &&
+export LC_ALL=en_US.UTF-8 ' \
+    $USERNAME &> /dev/null
+
+
+echo -e "\nInstall guix packages"
+echo -e "---------------------\n"
+
+
+echo "Installing the guix packages..."
+su -c "guix package -i $ESSENTIAL_GUIX_PACKAGES \ 
+$GUIX_PACKAGES" \
+   $USERNAME &> /dev/null
+
+
+echo -e "\nConfigure packages"
+echo -e "------------------\n"
+
+
+echo "Updating grub..."
 if [[ $HIDE_GRUB = true ]]; then
-/bin/cat <<EOM >/etc/default/grub
+    /bin/cat <<EOM >/etc/default/grub
 GRUB_CMDLINE_LINUX=""
 GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"
 GRUB_DEFAULT=0
@@ -200,49 +216,62 @@ EOM
 fi
 update-grub
 
-# Configure git
+
+echo "Configuring git..."
 git config --global core.editor emacs
 git config --global user.email $GIT_EMAIL
 git config --global user.name $GIT_EMAIL
-ln -sf  ~/.gitconfig /home/$USERNAME/.gitconfig
+cp -rf  ~/.gitconfig /home/$USERNAME/.gitconfig
 
-# Configure emacs
-wget -O ~/.emacs https://gitlab.com/RobinWils/dotfiles/raw/master/.emacs
-ln -sf ~/.emacs /home/$USERNAME/.emacs
 
-# Create init script for startx
-wget -O ~/.xinitrc https://gitlab.com/RobinWils/dotfiles/raw/master/.xinitrc
-ln -sf ~/.xinitrc /home/$USERNAME/.xinitrc
+echo "Configuring emacs..."
+wget \
+    -O ~/.emacs https://gitlab.com/RobinWils/dotfiles/raw/master/.emacs \
+    -q > /dev/null
+cp -rf ~/.emacs /home/$USERNAME/.emacs
+
+
+echo "Creating init script for startx..."
+wget \
+    -O ~/.xinitrc https://gitlab.com/RobinWils/dotfiles/raw/master/.xinitrc \
+    -q > /dev/null
+cp -rf  ~/.xinitrc /home/$USERNAME/.xinitrc
+
 
 # RICE FIREFOX
 # The problem with this part is that the location does not exists until
-# someone ran firefox. So this part is not configured yet.
+# someone runs firefox. So this part is not configured yet.
 # wget -O ~/.mozilla/firefox/*.default/chrome/userChrome.css \
-    # https://gitlab.com/RobinWils/dotfiles/raw/master/userChrome.css
+# https://gitlab.com/RobinWils/dotfiles/raw/master/userChrome.css
 
-# Replace the default host file
-wget -O /etc/hosts $HOSTS
 
-# Give our user ownership on their files
+echo "Replacing the default host file..."
+wget -O /etc/hosts \
+     $HOSTS -q \
+     > /dev/null
+
+
+echo "Making sure that the user owns his home..."
 chown -R $USERNAME /home/$USERNAME
 
-# Set the important vars for guix
-# I currently run a bash script with this after my system boots.
 
-# The first line is commented since we started the daemon already.
-# The other vars that are set are commented as well.
+#TODO: figure out of we need this
+# echo "Setting the important vars for guix..."
+
+# I currently run a bash script with this after my system boots..
+# The vars that are set are commented.
 # It is not commented in that other script that I mentioned.
 
-su -c '
-export GIO_EXTRA_MODULES=$HOME/.guix-profile/lib/gio/modules${GIO_EXTRA_MODULES:+:}$GIO_EXTRA_MODULES &&
-export GIT_SSL_CAINFO=$HOME/.guix-profile/etc/ssl/certs/ca-certificates.crt &&
-# export GUIX_LOCPATH=$HOME/.guix-profile/lib/locale &&
-# export INFOPATH=$HOME/.guix-profile/share/info${INFOPATH:+:}$INFOPATH &&
-# export LC_ALL=en_US.UTF-8 &&
+# su -c '
 # export PATH=$HOME/.guix-profile/bin:$PATH/.guix-profile/sbin${PATH:+:}$PATH &&
-# export SSL_CERT_DIR=$HOME/.guix-profile/etc/ssl/certs &&
-# export SSL_CERT_FILE=$HOME/.guix-profile/etc/ssl/certs/ca-certificates.crt &&
-export XDG_DATA_DIRS=$HOME/.guix-profile/share${XDG_DATA_DIRS:+:}$XDG_DATA_DIRS &&
-export X_XFCE4_LIB_DIRS=$HOME/.guix-profile/lib/xfce4${X_XFCE4_LIB_DIRS:+:}$X_XFCE4_LIB_DIRS
-' $USERNAME
+# export GIO_EXTRA_MODULES=$HOME/.guix-profile/lib/gio/modules${GIO_EXTRA_MODULES:+:}$GIO_EXTRA_MODULES &&
+# export GIT_SSL_CAINFO=$HOME/.guix-profile/etc/ssl/certs/ca-certificates.crt &&
+# # export GUIX_LOCPATH=$HOME/.guix-profile/lib/locale &&
+# # export LC_ALL=en_US.UTF-8 &&
+# # export SSL_CERT_DIR=$HOME/.guix-profile/etc/ssl/certs &&
+# # export SSL_CERT_FILE=$HOME/.guix-profile/etc/ssl/certs/ca-certificates.crt &&
+# export XDG_DATA_DIRS=$HOME/.guix-profile/share${XDG_DATA_DIRS:+:}$XDG_DATA_DIRS &&
+# export X_XFCE4_LIB_DIRS=$HOME/.guix-profile/lib/xfce4${X_XFCE4_LIB_DIRS:+:}$X_XFCE4_LIB_DIRS
+# ' $USERNAME
+echo -e "/nInstallation finished!/nEnjoy your new system!/n"
 exit 0
